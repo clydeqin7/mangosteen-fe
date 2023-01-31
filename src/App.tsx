871e10b7fb530b1cda07051c627ec0d89ad57790
clydeqin7 <clydeqin7@foxmail.com>
2023-01-31 16:02:13 +0800

    feat: Welcome页通用样式; 基础 CSS 样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { defineComponent, ref } from 'vue'; 
 import { RouterView } from 'vue-router'
+import './App.scss';
 
 export const App = defineComponent({
   setup() {
-   
-    return () => <>
-      <header>导航
-        <ul>
-          <li>
-            <router-link to='/'>Foo</router-link>
-          </li>
-          <li>
-            <router-link to='/bar'>Bar</router-link>
-          </li>
-        </ul>
-      </header>
-      <main>
-        <router-view></router-view>
-      </main>
-      <footer>页脚</footer>
-    </>
+    return () => <div>
+      <router-view></router-view>
+    </div>
   }
 })
