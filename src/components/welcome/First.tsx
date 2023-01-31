044990168d5a1452a963f22cb19f7d1dba752116
clydeqin7 <clydeqin7@foxmail.com>
2023-01-31 16:52:30 +0800

    feat: 欢迎页第一个页面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import s from './First.module.scss';
+import pig from "../../assets/icons/pig.svg";
 export const First = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>First</div>
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={pig} class={s.pig} />
+          <h2>会挣钱<br/>还要会省钱</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+          <RouterLink to="/welcome/2">下一步</RouterLink>
+          <RouterLink to='/start' >跳过</RouterLink>
+        </div>
+      </div>
     )
   }
 })
+<template>
