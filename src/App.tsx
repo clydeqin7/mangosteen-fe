b365cc52d280414f7110fb88e38deb65df6b4849
clydeqin7 <clydeqin7@foxmail.com>
2023-02-26 15:26:09 +0800

    fix: edge 浏览器不能全屏展示

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.scss';
 
 export const App = defineComponent({
   setup() {
-    return () => <div>
-      <router-view></router-view>
+    return () => <div class="page">
+      <RouterView />
     </div>
   }
 })
